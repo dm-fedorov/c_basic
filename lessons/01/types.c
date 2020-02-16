7ed9ec07292b3d6ffc0bb4550f79fac589a532da
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2020-02-16 22:45:38 +0300

    add type.c

---
+++ lessons/01/types.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(){
+
+  signed char ch = 126;
+
+  printf("%ld byte\n", sizeof(char));
+
+  printf("%d\n", CHAR_MIN); /* https://ru.wikipedia.org/wiki/Limits.h */
+  printf("%d\n", CHAR_MAX);
+
+  printf("%c\n", ch);
+  printf("%d\n", ch = ch + 2);
+
+  return 0;
+}
