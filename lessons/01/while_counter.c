9246f3a1f2bc05218a46c8e5c3e7c7222e093936
Дима Федоров <dmitriy.fedoroff@gmail.com>
2020-02-17 12:54:06 +0300

    Update while_counter.c

--- lessons/01/while_counter.c
+++ lessons/01/while_counter.c
@@ -3,7 +3,8 @@
 int main()
 {
    int counter, grade, total, average;
-
+   int n;
+   
    /* initialization phase */
    total = 0;
    counter = 1;
@@ -11,9 +12,12 @@ int main()
    /* processing phase */
    while(counter <= 10) {
       printf("Enter grade: ");
-      scanf("%d", &grade);
-      total = total + grade;
-      counter = counter + 1;
+      n = scanf("%d", &grade);
+      if(n != 1){
+         return 1; /* error */
+      }
+      total = total + grade; /* total += grade; */
+      counter = counter + 1; /* counter++; */
    }
 
    /* termination phase */
