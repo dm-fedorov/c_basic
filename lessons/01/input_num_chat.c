ffd6fe7167dc9d8b74bedbf2cc9f7e956f682db0
Dmitry Fedorov <info@dfedorov.spb.ru>
2020-02-21 13:48:46 +0300

    add source

---
+++ lessons/01/input_num_chat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    int n;
+
+    printf("Hello!\nWhat's your age?\n");
+    n = scanf("%d", &age);
+    if(n != 1){
+        return 1; /* exit with error */
+    }
+
+    printf("I thought you were older! You are %d years old!\n", age+2);
+    printf("Bye!\n");
+
+    return 0;
+}
