13903e13f61050ee9b583452bd09605d8f0f4695
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2020-02-22 22:49:20 +0300

    add average.c

---
+++ lessons/01/average.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    double a, b, c, d, e;
+    int n;
+    printf("Enter five numbers.\n");
+    n = scanf("%lf %lf %lf %lf %lf", &a, &b, &c, &d, &e); /* & - операция взятия адреса */
+    if (n != 5) {
+        printf("Error: wrong input.\n");
+        return 1;
+    }
+
+    printf("Average = %lf\n", (a + b + c + d + e)/5);
+    return 0;
+}
